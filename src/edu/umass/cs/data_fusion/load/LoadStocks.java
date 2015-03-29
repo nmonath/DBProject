0c7f69f93645ed423b922be18bdcc0ed3342ca7f
Nicholas Monath <nicholas.monath@gmail.com>
2015-03-29 00:57:37 -0400

    Changes to stock loader, FloatAttribute handles conversion

--- src/edu/umass/cs/data_fusion/load/LoadStocks.java
+++ src/edu/umass/cs/data_fusion/load/LoadStocks.java
@@ -1,19 +1,10 @@
 package edu.umass.cs.data_fusion.load;
 
-
-import edu.umass.cs.data_fusion.data_structures.Attribute;
 import edu.umass.cs.data_fusion.data_structures.AttributeType;
 
 public class LoadStocks extends LoadTSVFile {
-    
 
     protected String[] attributeNames = {"Percent Change", "Last Trading Price", "Open Price", "Change $", "Volume", "Today's High", "Today's Low", "Previous Close", "52wk High", "52wk Low", "Shares Outstanding", "P/E", "Market Cap", "Yield", "Dividend", "EPS"};
     private AttributeType[] attributeTypes = {AttributeType.FLOAT,AttributeType.FLOAT,AttributeType.FLOAT,AttributeType.FLOAT,AttributeType.FLOAT,AttributeType.FLOAT,AttributeType.FLOAT,AttributeType.FLOAT,AttributeType.FLOAT,AttributeType.FLOAT,AttributeType.FLOAT,AttributeType.FLOAT,AttributeType.FLOAT,AttributeType.FLOAT,AttributeType.FLOAT,AttributeType.FLOAT};
-    
-    @Override
-    protected Attribute getFloatAttributeFromString(String name, String rawValue) {
-        double value = Double.parseDouble(rawValue.replaceAll("[^\\.0-9]","")); // TODO: Work more on this regex. Remove everything except numbers and periods
-        return new Attribute(name,rawValue); // new FloatAttribute(name,rawValue,value) 
-    }
-    
+
 }
