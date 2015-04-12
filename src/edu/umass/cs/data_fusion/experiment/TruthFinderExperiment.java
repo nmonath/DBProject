21a1386976be0513221a1862bb0ff943193f8216
Nicholas Monath <nicholas.monath@gmail.com>
2015-04-12 13:16:15 -0400

    added truth finder experiment code

---
+++ src/edu/umass/cs/data_fusion/experiment/TruthFinderExperiment.java
@@ -0,0 +1,59 @@
+package edu.umass.cs.data_fusion.experiment;
+
+
+import edu.umass.cs.data_fusion.algorithm.TruthFinder;
+import edu.umass.cs.data_fusion.data_structures.RecordCollection;
+import edu.umass.cs.data_fusion.data_structures.Result;
+import edu.umass.cs.data_fusion.evaluation.EvaluationMetrics;
+import edu.umass.cs.data_fusion.load.LoadStocks;
+import edu.umass.cs.data_fusion.util.HTMLOutput;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class TruthFinderExperiment {
+    
+    /*
+    Path to data should point to the directory containing
+    the data folder which has a subdirectory called stock
+    containing:
+        clean_stock_rawdata
+        nasdaq_truth_golddata
+        pop_truth_golddata
+    */
+
+
+    // Usage TruthFinderExperiment <path-to-data-dir> <output-dir>
+    public static void main(String[] args) {
+
+        // CMD arguments
+        File dataPath = new File(args[0]);
+        File outputDir = new File(args[1]);
+
+
+        // Make output dir if it doesn't exist
+        outputDir.mkdirs();
+
+        // Load the data to fuse
+        LoadStocks loader = new LoadStocks();
+        RecordCollection collection = loader.load(new File(dataPath, "data/stock/clean_stock_rawdata"));
+
+        // Run the algorithm
+        TruthFinder tf = new TruthFinder();
+        ArrayList<Result> results = tf.execute(collection);
+
+        // Load the gold data
+        RecordCollection gold = loader.loadGold(new File("nasdaq_truth/stock-2011-07-01-nasdaq-com.txt"));
+
+        // Evaluate the data
+        EvaluationMetrics eval = new EvaluationMetrics(results, gold);
+        eval.calcMetrics();
+        eval.printResults();
+
+        // Write out the output
+        RecordCollection resultsCollection = tf.convert(results);
+        HTMLOutput.writeHTMLOutput(resultsCollection, gold, new File(outputDir, "report.html").getAbsolutePath(), true);
+        resultsCollection.writeToTSVFile(new File(outputDir, "output.tsv"), LoadStocks.names);
+        // TODO: Write Score to a file
+    }
+}
