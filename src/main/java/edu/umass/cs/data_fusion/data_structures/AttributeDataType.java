bbdd6b7c09b508620440bd9af30610dca7777d02
Nicholas Monath <nicholas.monath@gmail.com>
2015-04-18 15:04:01 -0400

    change to datatype file

--- src/main/java/edu/umass/cs/data_fusion/data_structures/AttributeDataType.java
+++ src/main/java/edu/umass/cs/data_fusion/data_structures/AttributeDataType.java
@@ -3,13 +3,15 @@ package main.java.edu.umass.cs.data_fusion.data_structures;
 
 // We can change this around with Manuel's code, but for now I added this in
 public enum AttributeDataType {
-    STRING,FLOAT;
+    STRING,FLOAT,AUTHOR_LIST,BAG_OF_WORDS;
     
     public static AttributeDataType fromString(String str) {
         if (str.equalsIgnoreCase("string"))
             return STRING;
         else if (str.equalsIgnoreCase("float"))
             return FLOAT;
+        else if (str.equalsIgnoreCase("author_list"))
+            return AUTHOR_LIST;
         else 
             return null;
     }
