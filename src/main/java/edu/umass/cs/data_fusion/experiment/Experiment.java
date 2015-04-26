2e832708fa32665816b282361419ebb14773854d
Nicholas Monath <nicholas.monath@gmail.com>
2015-04-26 15:36:44 -0400

    hyper parameter tuning

--- src/main/java/edu/umass/cs/data_fusion/experiment/Experiment.java
+++ src/main/java/edu/umass/cs/data_fusion/experiment/Experiment.java
@@ -10,6 +10,7 @@ import main.java.edu.umass.cs.data_fusion.load.LoadTSVFile;
 import main.java.edu.umass.cs.data_fusion.util.HTMLOutput;
 
 import java.io.File;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 
 public class Experiment {
@@ -21,6 +22,12 @@ public class Experiment {
     protected RecordCollection gold;
     protected File outputDir;
     protected LoadTSVFile loader;
+    
+    private double precision;
+    private double recall;
+    private double errorRate;
+    private double mad;
+    private double mnad;
     
     public Experiment(Algorithm algorithm, boolean evaluationWithSlack, LoadTSVFile loader, RecordCollection inputData, RecordCollection gold, File outputDir) {
         this.algorithm = algorithm;
@@ -52,14 +59,49 @@ public class Experiment {
         evaluator.calcMAD();
         evaluator.calcMNAD();
         evaluator.printResults();
+        precision = evaluator.getPrecision();
+        recall = evaluator.getRecall();
+        errorRate = evaluator.getErrorRate();
+        mad = evaluator.getMAD();
+        mnad = evaluator.getMNAD();
 
         // Write the output
         outputDir.mkdirs();
         RecordCollection resultsCollection = algorithm.convert(results);
         HTMLOutput.writeHTMLOutput(loader.getOrderedAttributeNames(), resultsCollection, gold, new File(outputDir, "report.html").getAbsolutePath(), true, evaluator);
         resultsCollection.writeToTSVFile(new File(outputDir, "output.tsv"), loader.getOrderedAttributeNames());
+        writeScoreFile(outputDir,evaluator.resultsString());
 
     }
-    
 
+    public double getPrecision() {
+        return precision;
+    }
+
+    public double getRecall() {
+        return recall;
+    }
+
+    public double getErrorRate() {
+        return errorRate;
+    }
+
+    public double getMad() {
+        return mad;
+    }
+
+    public double getMnad() {
+        return mnad;
+    }
+    
+    public void writeScoreFile(File outputDir, String scoreString) {
+        try {
+            PrintWriter writer = new PrintWriter(new File(outputDir, "score.txt"));
+            writer.println(scoreString);
+            writer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+    }
 }
