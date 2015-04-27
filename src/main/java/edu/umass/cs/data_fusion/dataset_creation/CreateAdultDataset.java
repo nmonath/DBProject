237ceaaefa4e5b989effe2ca9d5a979152f4ddbf
Nicholas Monath <nicholas.monath@gmail.com>
2015-04-27 10:52:05 -0400

    adult datasets

--- src/main/java/edu/umass/cs/data_fusion/dataset_creation/CreateAdultDataset.java
+++ src/main/java/edu/umass/cs/data_fusion/dataset_creation/CreateAdultDataset.java
@@ -1,11 +1,13 @@
 package main.java.edu.umass.cs.data_fusion.dataset_creation;
 
 import main.java.edu.umass.cs.data_fusion.data_structures.RecordCollection;
+import main.java.edu.umass.cs.data_fusion.data_structures.Source;
 import main.java.edu.umass.cs.data_fusion.data_structures.SyntheticSource;
 import main.java.edu.umass.cs.data_fusion.load.LoadAdult;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class CreateAdultDataset {
     
@@ -23,12 +25,21 @@ public class CreateAdultDataset {
         // Modification code
         CreateSyntheticDataset createSyntheticDataset = new CreateSyntheticDataset();
         
+        Random random = new Random(0);
+        
         //synthetic data sources
         ArrayList<SyntheticSource> sources = new ArrayList<SyntheticSource>();
-    	SyntheticSource source1 = new SyntheticSource("synth1", 0.1, 1);
-    	SyntheticSource source2 = new SyntheticSource("synth2", 1, 3);        
-    	sources.add(source1);
-    	sources.add(source2);
+        for (int i = 0; i < 100; i ++ ) {
+            double theta = random.nextDouble();
+            double sigma = random.nextDouble()*2.0;
+            sources.add(new SyntheticSource(String.format("synth_%d_%g_%g",i,theta,sigma), theta,sigma));
+        }
+        
+        
+//    	SyntheticSource source1 = new SyntheticSource("synth1", 0.1, 1);
+//    	SyntheticSource source2 = new SyntheticSource("synth2", 1, 3);
+//    	sources.add(source1);
+//    	sources.add(source2);
     	
         RecordCollection noisyData = createSyntheticDataset.createModifiedDataset(collection, sources);
         
